9bbd0d74a17d721318ceef170958a3ae44608253
tokka! <167693150+pimaky@users.noreply.github.com>
2025-05-04 18:30:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,119 +1,77 @@
-let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp
 
 tg.expand()
 
 tg.MainButton.textColor = "#FFFFFF"
-tg.MainButton.color = "#2cab37"
-
-let btn1 = document.querySelector('#btn1')
-let btn2 = document.querySelector('#btn2')
-let btn3 = document.querySelector('#btn3')
-let btn4 = document.querySelector('#btn4')
-
-let price = 0
-let name = ""
-let phone = ""
-let e-mail = ""
+tg.MainButton.color = "#FF8979"
 
 let items = {
-    MS: 0,
-    RK: 0,
-    KK: 0,
-    VA: 0
+    carrot: 0,
+    potato: 0,
+    cucumber: 0,
+    tomato: 0
 }
+let price = 0
 
-let usercard = document.querySelector('#usercard')
-let username = document.querySelector('#user-name')
-let userphone = document.querySelector('#user-phone')
-let useremail = document.querySelector('#user-email')
+let btn1 = document.querySelector("#btn1")
+let btn2 = document.querySelector("#btn2")
+let btn3 = document.querySelector("#btn3")
+let btn4 = document.querySelector("#btn4")
+
+let usercard = document.querySelector("#usercard")
 
 function create_li(text) {
     let li = document.createElement("li")
-    li.innerHTML = Text
+    li.innerHTML = text
     usercard.appendChild(li)
 }
 
+function update_orders() {
+    for (let item in items) {
+        if (items[item] != 0) {
+            create_li(item + ": " + items[item])
+        }
+    }
+}
+
 btn1.onclick = () => {
-    items['MS'] += 1
-    price += 100
+    items['carrot'] += 1
+    price += 5
     update_orders()
 }
 
 btn2.onclick = () => {
-    items['RK'] += 1
-    price += 50
+    items['potato'] += 1
+    price += 3
     update_orders()
 }
 
 btn3.onclick = () => {
-    items['KK'] += 1
-    price += 20
+    items['cucumber'] += 1
+    price += 4
     update_orders()
 }
 
 btn4.onclick = () => {
-    items['VA'] += 1
-    price += 30
+    items['tomato'] += 1
+    price += 6
     update_orders()
 }
-})
 
-let submit = document.querySelector('#submit')
+let submit = document.querySelector("#submit")
 
 submit.onclick = () => {
-    tg.MainButton.setText("нажмите на кнопку для оформления заказа")
+    tg.MainButton.setText("Нажмите на кнопку для оформления заказа!")
     tg.MainButton.show()
 }
 
-function update_orders() {
-    usercard.innerHTML = "Ваши заказы: "
-    if (name != "") {
-        create_li("имя: " + name)
-    }
-    if (phone != "") {
-        create_li("телефон: " + phone)
-    }
-    if (email != "") {
-        create_li("почта: " + email)
-    }
+Telegram.WebApp.onEvent("mainButtonClicked", function() {
+    result += "Ваши заказы: \n"
     for (let item in items) {
         if (items[item] != 0) {
-            let li = document.createElement("li")
-            li.innerHTML = item + ": " + items[item]
-            usercard.appendChild(li)
+            result += item + ": " + items[item] + "\n"
         }
     }
-}
-
-
-username.onchange() = () => {
-    name = username.value
-}
-userphone.onchange() = () => {
-    phone = userphone.value
-}
-useremail.onchange() = () => {
-    email = useremail.value
-}
-
-Telegram.WebApp.onEvent("mainButtonClicked", function) {
-    result = ""
-    if (name != "") {
-        create_li("имя: " + name)
-    }
-    if (phone != "") {
-        create_li("телефон: " + phone)
-    }
-    if (email != "") {
-        create_li("почта: " + email)
-    }
-
-    result += "ваши заказы: \n"
-    for (let item in items) {
-        if (items[item] != 0) {
-            result += item + " : " + items[items] + "\n"
-        }
-    }
-    result += "\n\n с вас " + price + "$"
+    result += "\n\nС вас " + price + "$"
     tg.sendData(result)
-}
+})
